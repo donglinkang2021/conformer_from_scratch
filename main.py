c2dae092986b463595ae1fa7faa5dab7b4fbb1c1
Linkdom <donglinkang2021@163.com>
2023-11-27 17:05:08 +0800

    modify the code before and learn what is "wer"

--- main.py
+++ main.py
@@ -33,89 +33,33 @@ def main(args):
     else:
         args.initial_epoch = 0
 
-    # Load Encoder Only
-    if args.initial_epoch_encoder is not None:
-        model.load_encoder(config["training_params"]["callback_path_encoder"] + "checkpoints_" + str(args.initial_epoch_encoder) + ".ckpt")
-
-    # Load LM
-    if args.initial_epoch_lm:
-
-        # Load LM Config
-        with open(config["decoding_params"]["lm_config"]) as json_config:
-            config_lm = json.load(json_config)
-
-        # Create LM
-        model.lm = create_model(config_lm).to(device)
-
-        # Load LM
-        model.lm.load(config_lm["training_params"]["callback_path"] + "checkpoints_" + str(args.initial_epoch_lm) + ".ckpt")
-
     # Model Summary
     model.summary()
 
     # Load Dataset
-    dataset_train, dataset_val = load_datasets(config["training_params"], config["tokenizer_params"], args)
+    dataset_train, dataset_val = load_datasets(
+        config["training_params"], 
+        config["tokenizer_params"], 
+        args
+    )
 
-    ###############################################################################
-    # Modes
-    ###############################################################################
-
-    # Stochastic Weight Averaging
-    if args.swa:
-
-        model.swa(dataset_train, callback_path=config["training_params"]["callback_path"], start_epoch=args.swa_epochs[0] if args.swa_epochs else None, end_epoch=args.swa_epochs[1] if args.swa_epochs else None, epochs_list=args.swa_epochs_list, update_steps=args.steps_per_epoch, swa_type=args.swa_type)
-
+   
     # Training
-    elif args.mode.split("-")[0] == "training":
+    model.fit(
+        dataset_train, 
+        config["training_params"]["epochs"], 
+        dataset_val=dataset_val, 
+        val_steps=args.val_steps, 
+        verbose_val=args.verbose_val, 
+        initial_epoch=int(args.initial_epoch), 
+        callback_path=config["training_params"]["callback_path"], 
+        steps_per_epoch=args.steps_per_epoch,
+        mixed_precision=config["training_params"]["mixed_precision"],
+        accumulated_steps=config["training_params"]["accumulated_steps"],
+        saving_period=args.saving_period,
+        val_period=args.val_period
+    )
 
-        model.fit(dataset_train, 
-            config["training_params"]["epochs"], 
-            dataset_val=dataset_val, 
-            val_steps=args.val_steps, 
-            verbose_val=args.verbose_val, 
-            initial_epoch=int(args.initial_epoch), 
-            callback_path=config["training_params"]["callback_path"], 
-            steps_per_epoch=args.steps_per_epoch,
-            mixed_precision=config["training_params"]["mixed_precision"],
-            accumulated_steps=config["training_params"]["accumulated_steps"],
-            saving_period=args.saving_period,
-            val_period=args.val_period)
-
-    # Evaluation
-    elif args.mode.split("-")[0] == "validation" or args.mode.split("-")[0] == "test":
-
-        # Gready Search Evaluation
-        if args.gready or model.beam_size is None:
-
-            print("Gready Search Evaluation")
-            wer, _, _, _ = model.evaluate(dataset_val, eval_steps=args.val_steps, verbose=args.verbose_val, beam_size=1, eval_loss=args.eval_loss)
-            print("Geady Search WER : {:.2f}%".format(100 * wer))
-        
-        # Beam Search Evaluation
-        else:
-            # TODO: delete this
-            print("Beam Search Evaluation")
-            wer, _, _, _ = model.evaluate(dataset_val, eval_steps=args.val_steps, verbose=args.verbose_val, beam_size=model.beam_size, eval_loss=False)
-            print("Beam Search WER : {:.2f}%".format(100 * wer))
-    
-    # Eval Time
-    elif args.mode.split("-")[0] == "eval_time":
-
-        print("Model Eval Time")
-        inf_time = model.eval_time(dataset_val, eval_steps=args.val_steps, beam_size=1, rnnt_max_consec_dec_steps=args.rnnt_max_consec_dec_steps, profiler=args.profiler)
-        print("eval time : {:.2f}s".format(inf_time))
-
-    elif args.mode.split("-")[0] == "eval_time_encoder":
-
-        print("Encoder Eval Time")
-        enc_time = model.eval_time_encoder(dataset_val, eval_steps=args.val_steps, profiler=args.profiler)
-        print("eval time : {:.2f}s".format(enc_time))
-
-    elif args.mode.split("-")[0] == "eval_time_decoder":
-
-        print("Decoder Eval Time")
-        dec_time = model.eval_time_decoder(dataset_val, eval_steps=args.val_steps, profiler=args.profiler)
-        print("eval time : {:.2f}s".format(dec_time))
 
 
 if __name__ == "__main__":
@@ -123,31 +67,14 @@ if __name__ == "__main__":
     # Args
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--config_file",          type=str,   default="ConformerCTCSmall.json",           help="Json configuration file containing model hyperparameters")
-    parser.add_argument("-m", "--mode",                 type=str,   default="training",                                 help="Mode : training, validation-clean, test-clean, eval_time-dev-clean, ...")
     parser.add_argument("-i", "--initial_epoch",        type=str,   default=None,                                       help="Load model from checkpoint")
-    parser.add_argument("--initial_epoch_lm",           type=str,   default=None,                                       help="Load language model from checkpoint")
-    parser.add_argument("--initial_epoch_encoder",      type=str,   default=None,                                       help="Load model encoder from encoder checkpoint")
-    parser.add_argument("-p", "--prepare_dataset",      action="store_true",                                            help="Prepare dataset for training")
-    parser.add_argument("-j", "--num_workers",          type=int,   default=1,                                          help="Number of data loading workers")
-    parser.add_argument("--create_tokenizer",           action="store_true",                                            help="Create model tokenizer")
     parser.add_argument("--batch_size_eval",            type=int,   default=8,                                          help="Evaluation batch size")
     parser.add_argument("--verbose_val",                action="store_true",                                            help="Evaluation verbose")
     parser.add_argument("--val_steps",                  type=int,   default=None,                                       help="Number of validation steps")
     parser.add_argument("--steps_per_epoch",            type=int,   default=None,                                       help="Number of steps per epoch")
-    parser.add_argument("--world_size",                 type=int,   default=torch.cuda.device_count(),                  help="Number of available GPUs")
     parser.add_argument("--cpu",                        action="store_true",                                            help="Load model on cpu")
-    parser.add_argument("--show_dict",                  action="store_true",                                            help="Show model dict summary")
-    parser.add_argument("--swa",                        action="store_true",                                            help="Stochastic weight averaging")
-    parser.add_argument("--swa_epochs",                 nargs="+",  default=None,                                       help="Start epoch / end epoch for swa")
-    parser.add_argument("--swa_epochs_list",            nargs="+",  default=None,                                       help="List of checkpoints epochs for swa")
-    parser.add_argument("--swa_type",                   type=str,   default="equal",                                    help="Stochastic weight averaging type (equal/exp)")
-    parser.add_argument("--parallel",                   action="store_true",                                            help="Parallelize model using data parallelization")
-    parser.add_argument("--rnnt_max_consec_dec_steps",  type=int,   default=None,                                       help="Number of maximum consecutive transducer decoder steps during inference")
-    parser.add_argument("--eval_loss",                  action="store_true",                                            help="Compute evaluation loss during evaluation")
-    parser.add_argument("--gready",                     action="store_true",                                            help="Proceed to a gready search evaluation")
     parser.add_argument("--saving_period",              type=int,   default=1,                                          help="Model saving every 'n' epochs")
     parser.add_argument("--val_period",                 type=int,   default=1,                                          help="Model validation every 'n' epochs")
-    parser.add_argument("--profiler",                   action="store_true",                                            help="Enable eval time profiler")
 
     # Parse Args
     args = parser.parse_args()
